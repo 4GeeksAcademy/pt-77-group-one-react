9741c3a0c8d098c4979e0e3f88401333b990a952
aaransmith11 <aaransmith0817@gmail.com>
2025-03-11 00:22:23 +0000

    footer completed

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from './Footer'
 
 const Home = () => {
 	return (
@@ -10,6 +11,7 @@ const Home = () => {
 			{/*  cards */}
 			
 			{/*  footer */}
+			<Footer/>
 			
 		</div>
 	);
