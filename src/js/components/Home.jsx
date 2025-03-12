e4e9f06aee90950b3ff2c1a40c58249d565500e5
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-03-12 20:36:43 +0000

    lorem Ipsum added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -11,10 +11,18 @@ const Home = () => {
 			
 			{/*  card */}
 			<div className="d-flex">
-				<Card img={"https://townsquare.media/site/295/files/2021/05/10-Years-collage.jpg"}  title={"Most Popular Bands of 2025"} content={""} />
-				<Card img={"https://ca-times.brightspotcdn.com/dims4/default/a90d9dc/2147483647/strip/true/crop/2575x1716+0+0/resize/2000x1333!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F87%2F4b%2F9873494e403bba58213e618155b4%2Fgrammy-illustration-2.jpg"} title={"Most popular artists of 2025"} content={""}/>
-				<Card img={"https://i8.amplience.net/i/naras/Girl-Groups-Editorial-Collage_08272024_Hero-1644x925.jpg"} title={"Best International Groups"} content={""}/>
-				<Card img={"https://notjustok.com/wp-content/uploads/2024/12/The-20-best-Afrobeats-artists-of-2024-scaled.jpg"} title={"Best AfroBeats Artists"} content={""}/>
+				<Card img={"https://townsquare.media/site/295/files/2021/05/10-Years-collage.jpg"}  
+				title={"Most Popular Bands of 2025"} 
+				content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."} />
+				<Card img={"https://ca-times.brightspotcdn.com/dims4/default/a90d9dc/2147483647/strip/true/crop/2575x1716+0+0/resize/2000x1333!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F87%2F4b%2F9873494e403bba58213e618155b4%2Fgrammy-illustration-2.jpg"}
+				 title={"Most popular artists of 2025"} 
+				 content={"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+				<Card img={"https://i8.amplience.net/i/naras/Girl-Groups-Editorial-Collage_08272024_Hero-1644x925.jpg"} 
+				title={"Best International Groups"} 
+				content={"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}/>
+				<Card img={"https://notjustok.com/wp-content/uploads/2024/12/The-20-best-Afrobeats-artists-of-2024-scaled.jpg"} 
+				title={"Best AfroBeats Artists"} 
+				content={"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
 			</div>
 			
 			
