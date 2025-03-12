5d4c3db80c029d34e30a15982662a9127a873f52
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-03-12 22:28:08 +0000

    navbar font

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
             <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div class="container-fluid justify-content-center">
-                    <a class="navbar-brand" href="#">Popular Music</a>
+                    <a class="navbar-brand fw-bold" href="#">POPular Music</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
